b27f2158d8363e7ee1b49cb33f5cfbf119937b75
Sapphire Mason-Brown <saphmb@gmail.com>
2017-08-02 15:01:24 +0100

    Implemented methods to prevent temperature adjustments beyond maximum and minimum

--- jasmine-standalone-2.7.0/spec/ThermostatSpec.js
+++ jasmine-standalone-2.7.0/spec/ThermostatSpec.js
@@ -1,26 +1,45 @@
+'use strict';
+
 describe ('Thermostat', function() {
+
+  var thermostat;
 
   beforeEach(function() {
     thermostat = new Thermostat();
   });
 
   describe('when is is initialized', function() {
-    it('should have a temperature is 20 degreese', function () {
+    it('should have a temperature of 20º', function () {
       expect(thermostat.getTemp()).toEqual(20);
     });
   });
 
   describe('when the user adjusts the temperature', function(){
     it('should decrease the temperature by 1º',function(){
-      initialTemp = thermostat.getTemp();
+      var initialTemp = thermostat.getTemp();
       thermostat.down();
       expect(thermostat.getTemp()).toEqual(initialTemp - 1);
     });
 
     it('should increase the temperature by 1º',function(){
-      initialTemp = thermostat.getTemp();
+      var initialTemp = thermostat.getTemp();
       thermostat.up();
       expect(thermostat.getTemp()).toEqual(initialTemp + 1);
+    });
+
+    it('has a minimum temperature of 10º', function() {
+      for(var i = 1; i < 40; i++) {
+        thermostat.down();
+      };
+      expect(thermostat.getTemp()).toEqual(10);
+    });
+
+    it('has a maximum temperature of 25º when in power-saving mode', function() {
+      console.log("Hi");
+      for(var i = 1; i < 40; i++) {
+        thermostat.up();
+      };
+      expect(thermostat.getTemp()).toEqual(25);
     });
 
   });
--- jasmine-standalone-2.7.0/src/Thermostat.js
+++ jasmine-standalone-2.7.0/src/Thermostat.js
@@ -1,5 +1,9 @@
+'use strict';
+
 function Thermostat () {
   this.DEFAULT_TEMP = 20;
+  this.MINIMUM_TEMP = 10;
+  this.MAX_TEMP_PS_MODE_ON = 25;
   this.temp = this.DEFAULT_TEMP;
 };
 
@@ -8,9 +12,13 @@ Thermostat.prototype.getTemp = function() {
 };
 
 Thermostat.prototype.down = function() {
+  if (this.getTemp() === this.MINIMUM_TEMP) {
+    return }
   this.temp -= 1;
 };
 
 Thermostat.prototype.up = function() {
+  if (this.getTemp() === this.MAX_TEMP_PS_MODE_ON) {
+    return }
   this.temp += 1;
 };
